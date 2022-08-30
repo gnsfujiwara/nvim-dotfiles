f6b8798b163e261806c701029480ceaea86777ed
Gustavo Vinícios de Souza <gnsfujiwara@disroot.org>
2022-08-29 22:49:07 -0300

    Setup formatting for lua-language-server

--- lua/lsp/settings/sumneko_lua.lua
+++ lua/lsp/settings/sumneko_lua.lua
@@ -15,6 +15,15 @@ return {
           [vim.fn.expand '$VIMRUNTIME/lua/vim/lsp'] = true,
         },
       },
+      format = {
+        enable = true,
+        defaultConfig = {
+          indent_style = 'space',
+          indent_size = '2',
+          quote_style = 'single',
+          call_arg_parentheses = 'unambiguous_remove_string_only',
+        },
+      }
     },
   },
 }
